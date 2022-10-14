b2dee55e46922d7422f80b745bb305630d7aaaca
Jeremy Bukofzer <jlbukofzer@gmail.com>
2022-10-13 20:11:48 -0500

    adds logic to stick navbar to top of screen

--- assets/script.js
+++ assets/script.js
@@ -104,3 +104,15 @@ function renderHarvard()
 // }
 
 
+window.onscroll = function() {stickNav()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function stickNav() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
