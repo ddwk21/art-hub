3664ea974c62419d456edb7babe0cc842701fc82
Jason <jajones414@gmail.com>
2022-10-17 21:43:31 -0500

    Corrected append paths.

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,7 @@ let harvardWorking;
 let imageEl;
 const harvardKey = "41920f1f-a0a4-40cf-b3fb-3ce184ea6dc1";
 const harvardArt = "https://api.harvardartmuseums.org/object";
-let elementCount=0;
+let elementCount = 0;
 let scrollLoaded;
 
 const harvardPages = 24170;
@@ -38,9 +38,9 @@ function harvardFetch() {
     //Filter function only allows in records that have an image included with their data, then adds them to the array harvardworking
 
     //call renderHarvard function
-  
+
     .then(() => renderHarvard())
-    .finally(() => scrollLoaded = true)
+    .finally(() => (scrollLoaded = true));
 }
 
 //random number between 0 and 400k
@@ -75,16 +75,14 @@ harvardFetch();
 // add counter
 // const otherImage = imageEl.appendChild('img')
 function renderHarvard() {
-
-
   if (harvardWorking.length <= 1) {
     harvardFetch();
     return;
-} else {
+  } else {
     addContent();
     //randomly select an item from harvardworking array
     let art = harvardWorking[Math.floor(Math.random() * harvardWorking.length)];
-    console.log("art", art)
+    console.log("art", art);
     // if (art.classificationid === 17 || !art.title || !art.people || !art.dated || !art.culture) {
     //     console.log("start over")
     //     console.log(art.people)
@@ -92,12 +90,12 @@ function renderHarvard() {
     //   return;
     // }
 
-    if (elementCount >= 1){
-      imageEl = document.getElementById("image"+elementCount)
+    if (elementCount >= 1) {
+      imageEl = document.getElementById("image" + elementCount);
     }
 
     imageEl.src = art.primaryimageurl;
-    
+
     // if (!! art.images) {
     //   //logic here
     //   for (let i = art.images.length - 1; i > 0; i--) {
@@ -111,76 +109,79 @@ function renderHarvard() {
     //   }
     // }
     //CHECK CLASSIFICATION HERE. IF PHOTO, CALL HARVARD FETCH AGAIN, RETURN FUNCTION
-    if (!!art.title)$("#art-info"+elementCount).append(`<p>Title: ${art.title}</p>`);
+    if (art.title)
+      $("#info" + elementCount).append(`<p>Title:` + art.title + "</p>");
+    console.log($(body));
 
-    if (!!art.people.name)$("#art-info"+elementCount).append(`<p>Name: ${art.people[0].name}</p>`);
+    if (art.people.name)
+      $("#info" + elementCount).append(`<p>Name: ${art.people[0].name}</p>`);
 
-    if (!!art.people.role)$("#art-info"+elementCount).append(`<p>Role: ${art.people[0].role}</p>`);
+    if (art.people.role)
+      $("#info" + elementCount).append(`<p>Role: ${art.people[0].role}</p>`);
 
-    if (!!art.dated)$("#art-info"+elementCount).append(`<p>Dated: ${art.dated}</p>`);
+    if (art.dated)
+      $("#info" + elementCount).append(`<p>Dated: ${art.dated}</p>`);
 
-    if (!!art.dated)$("#art-info"+elementCount).append(`<p>Culture: ${art.culture}</p>`)
+    if (art.dated)
+      $("#info" + elementCount).append(`<p>Culture: ${art.culture}</p>`);
     // $("#info").append(`<p>Medium: ${art.medium}</p>`);
     // using the console.log to filter through images that fit the criteria.
     // if (!! art.people[0].name){
-        console.log(
-            art.people[0].name,
-            art.people[0].role,
-            art.dated,
-            art.culture,
+    console.log(
+      art.people[0].name,
+      art.people[0].role,
+      art.dated,
+      art.culture,
       art.medium
     );
     console.log(art.images); // Display amount of images attached to file.
     console.log(art);
-    
-  // } else {
-}
+
+    // } else {
+  }
   // }
-  
+
   // exclude classification:17
 
   console.log(harvardWorking);
 
-
-  
   //trying to set the image from index html to the image of the record from the array
-  
+
   //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   // PUT THIS IN AN ELSE FUNCTION, SO IF THERE ARE NO ADDITIONAL IMAGES JUST USE PRIMARY
   //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  
-  
 }
 
+function addContent() {
+  elementCount++;
+  //generate new frame div
+  $("#body").append(
+    '<div class="art-container" id = "art-container' + elementCount + '"></div>'
+  );
 
+  //generate new img element
+  $("#art-container" + elementCount).append(
+    '<img class="mx-auto art-image my-11" id = "image' +
+      elementCount +
+      '" alt="Art Image"/>'
+  );
 
-function addContent()
-{
-    elementCount++
-    //generate new frame div
-    $('#body').append('<div class="art-container" id = "art-container'+elementCount+'"></div>')
+  $("#art-container" + elementCount).append(
+    '<div class="overlay"><div class="art-info max-w-1/2" id="info' +
+      elementCount +
+      '"></div></div>'
+  );
 
-    //generate new img element
-    $('#art-container'+elementCount).append('<img class="mx-auto art-image my-11" id = "image'+elementCount+'" alt="Art Image"/>')
+  //append new art-info element to same element as above, give art info a unique ID using the same method as above "art-info"+element count
+  //make sure where you are adding info to the element, you also use this unique id. Same methodology
 
-    $('#'+elementCount).append('<div class="overlay"><div class="art-info max-w-1/2" id="info"></div></div>')
-
-
-
-    //append new art-info element to same element as above, give art info a unique ID using the same method as above "art-info"+element count
-    //make sure where you are adding info to the element, you also use this unique id. Same methodology
-    
-
-    //assign element to variable, pass element to rendering/fetching functions
+  //assign element to variable, pass element to rendering/fetching functions
 }
-function fetchMaster()
-{
-    let toggle = Math.floor(Math.random())
+function fetchMaster() {
+  let toggle = Math.floor(Math.random());
 
-    if(toggle) metFetch();
-
-    else harvardFetch();
-
+  if (toggle) metFetch();
+  else harvardFetch();
 }
 
 function handleScroll() {
@@ -202,17 +203,17 @@ window.addEventListener("scroll", handleScroll);
 
 //if close enough to last child of body, run some rendering logic
 
-
-window.onscroll = function() {stickNav()};
+window.onscroll = function () {
+  stickNav();
+};
 
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
 function stickNav() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    navbar.classList.add("sticky");
   } else {
     navbar.classList.remove("sticky");
   }
 }
-
